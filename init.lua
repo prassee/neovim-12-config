447a5368cbf912cc56250ad2da42a9c39060816e
Prasanna <prassee@prassee.me>
2025-12-30 08:43:40 +0530

    added kanban plugin & board

--- init.lua
+++ init.lua
@@ -473,7 +473,35 @@ vim.keymap.set("n", "<leader>O", function()
 end, { desc = "OpenCode", unpack(opts) })
 
 vim.keymap.set("n", "<leader>t", ":ToggleTerm direction=float<CR>", opts)
+--:
 
+--:
+vim.pack.add({
+	{ src = "https://github.com/viniciusteixeiradias/kanban.nvim" },
+})
+require("kanban").setup({
+	file = {
+		path = nil, -- Custom path (nil = auto-detect)
+		name = "TODO.md", -- Default filename
+		create_if_missing = true,
+	},
+	default_columns = { "Backlog", "In Progress", "Done" },
+	window = {
+		width = 0.8,
+		height = 0.8,
+		border = "rounded",
+	},
+	highlights = {
+		column_header = { bold = true, fg = "#888888" },
+		column_header_active = { bold = true, fg = "#ffffff", bg = "#3a3a3a" },
+		task = { default = true },
+		task_active = { fg = "#000000", bg = "#7dd3fc", bold = true },
+		task_done = { strikethrough = true, fg = "#666666" },
+		separator = { fg = "#444444" },
+	},
+	auto_refresh_buffers = true, -- Refresh open markdown buffers on changes
+	on_complete_move_to = "Done", -- Target column when checked (nil to disable)
+})
 --:
 
 --: Fzf lua
