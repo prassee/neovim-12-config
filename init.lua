346a3b701b2d9873938c4dc0fc881fb6739df737
prasanna <ID+username@users.noreply.github.com>
2026-04-06 09:26:06 +0530

    Add plenary.nvim plugin

--- init.lua
+++ init.lua
@@ -141,6 +141,7 @@ vim.pack.add({
 	"https://github.com/github/copilot.vim",
 	"https://github.com/numToStr/Comment.nvim",
 	"https://github.com/lewis6991/gitsigns.nvim",
+	"https://github.com/nvim-lua/plenary.nvim",
 	"https://github.com/NeogitOrg/neogit",
 	"https://github.com/dangooddd/pyrepl.nvim",
 })
